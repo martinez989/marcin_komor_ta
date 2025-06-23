de8d60164f6a662512f42e0f6771363e01d3ced1
Marcin Komor <komor.marcin@gmail.com>
2025-06-23 20:29:46 +0200

    fix: duplicated output

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,6 @@ output "ecs_cluster_name" {
   value       = aws_ecs_cluster.main.name
 }
 
-output "ecs_service_name" {
-  description = "The name of the ECS service"
-  value       = aws_ecs_service.app.name
-}
-
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.app_repo.repository_url
